5f19c7c8e9f35b160e66d44e16c182c70645febf
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-10-17 14:37:25 +0300

    add users

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,4 +4,4 @@ $myvhosts = hiera('apache::vhosts', {})
 create_resources('apache::vhost', $myvhosts)
 
 $node_users = hiera_hash('users')
-create_resources(user, $users, {})
+create_resources(user, $node_users, {})
