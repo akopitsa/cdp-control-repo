e5299a1e6bbbf1c3e26a792dd1871d794474f3cb
Andrey Kopitsa <andrey.kopitsa@gmail.com>
2017-12-14 18:19:55 +0200

    add

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,8 +9,6 @@ create_resources('apache::vhost', $myvhosts)
 $mod = hiera('apache::mod', {})
 create_resources('apache::mod', $mod)
 
-$php = hiera('php', {})
-create_resources('php', $php)
 
 $node_users = hiera_hash('users')
 create_resources(user, $node_users, {})
